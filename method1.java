9c8b4afd3e603c4a9350c3dfffc40fe433ad24b2
qa191120 <74743291+qa191120@users.noreply.github.com>
2021-01-04 18:17:24 +0200

    Update method1.java

--- method1.java
+++ method1.java
@@ -19,10 +19,15 @@ public class Main
         // 1.1 write yourslef one method
         // 2. add println inside ach method (be original)
         // 3. debug using step over and step into
+        // 4. inside main write 3 lines of code , for example
+        //     input a number and print it
+        //     then ... select the 3 lines of code
+        //     and extract a methods from them...
     }
 
     private static void wakeUpInTheMorning() {
         System.out.println("Waking up ... i'm so tired...");
     }
+
 
 }
