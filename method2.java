cd519e46a7d61708e1ae9afc84d2472ccae417df
qa191120 <74743291+qa191120@users.noreply.github.com>
2021-01-04 19:30:58 +0200

    Create method2.java

---
+++ method2.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args)  {
+        // 1. write a function that prints 10 random numbers
+        // 2. write a function that inputs 10 numbers from user into an array
+        //    and prints the average
+        // 3. write a function that input two numbers and print their sum
+        // 4. write a function that input user name and tells him hello
+        //    now ... create a "for" loop and call this function 3 times...
+        // **etgar: write a function that prints the current date
+        // make sure to give your functions a clear name
+        // call of these functions from main
+    }
+
+
+
+}
