3873196fdeb6d0b36214d5bb7fa929d48c9e8aaf
qa191120 <74743291+qa191120@users.noreply.github.com>
2021-01-04 18:16:09 +0200

    Create method1.java

---
+++ method1.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main
+{
+    // 1
+    public static void main(String[] args)  {
+
+        wakeUpInTheMorning();
+        //GoToCar();
+        //driveToWork();
+        //goToDinner();
+        //goToMovie();
+        //goToSleep();
+
+        // 1 extract methods auto for all except 1
+        // 1.1 write yourslef one method
+        // 2. add println inside ach method (be original)
+        // 3. debug using step over and step into
+    }
+
+    private static void wakeUpInTheMorning() {
+        System.out.println("Waking up ... i'm so tired...");
+    }
+
+}
